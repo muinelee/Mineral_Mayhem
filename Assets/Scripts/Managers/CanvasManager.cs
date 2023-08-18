05177340fccc350f6c45954557fc01381b25a605
MuinelliTFS <ivan.lee@torontofilmschool.ca>
2023-08-17 20:30:46 -0600

    Added a Canvas Manager Script

---
+++ Assets/Scripts/Managers/CanvasManager.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+public class CanvasManager : MonoBehaviour
+{
+    [Header("Buttons")]
+    public Button startButton;
+    public Button settingsButton;
+    public Button backButton;
+    public Button quitButton;
+    public Button returnToMenuButton;
+    public Button resumeGame;
+
+    [Header("Menus")]
+    public GameObject mainMenu;
+    public GameObject settingsMenu;
+
+    [Header("Text")]
+    public Text volSliderText;
+
+    [Header("Slider")]
+    public Slider volSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Checks to see if these objects exist so that we avoid NULL references
+        if (startButton)
+            startButton.onClick.AddListener(StartGame);
+
+        if (settingsButton)
+            settingsButton.onClick.AddListener(ShowSettingsMenu);
+
+        if (backButton)
+            backButton.onClick.AddListener(ShowMainMenu);
+
+        if (quitButton)
+            quitButton.onClick.AddListener(Quit);
+
+        if (volSlider)
+        {
+            // Passing an anonymous function with AddListner(value) =>
+            volSlider.onValueChanged.AddListener((value) => OnSliderValueChanged(value));
+
+            if (volSliderText)
+            { 
+                volSliderText.text = volSlider.value.ToString();
+            }
+
+            OnSliderValueChanged(volSlider.value);
+        }
+
+    }
+
+    private void StartGame()
+    {
+        // Need to load the main game scene
+        // SceneManager.LoadScene("Game");
+    }
+
+    void ShowSettingsMenu()
+    {
+        mainMenu.SetActive(false);
+        settingsMenu.SetActive(true);
+    }
+
+    void Quit()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+
+    void OnSliderValueChanged(float value)
+    {
+        float step = 0.1f;
+        value = Mathf.Round(value / step) * step;
+        volSliderText.text = (value * 100).ToString();
+        
+        // Need to implement AudioManager, then uncomment next line
+        // AudioManager.Instance.SetVolume(value);
+    }
+
+    void ShowMainMenu()
+    {
+        mainMenu.SetActive(true);
+        settingsMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
