3b3b1ff917f654393ec7bb4168806c6b0c00be5d
rcuartero <106171055+rcuartero@users.noreply.github.com>
2023-09-25 20:57:53 -0400

    Implemented Cliff detection, gravity, and deathbox

--- Assets/Scripts/Level/DeathWall.cs
+++ Assets/Scripts/Level/DeathWall.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 
 public class DeathWall : MonoBehaviour
 {
-    
     private void OnTriggerEnter(Collider other) 
     {
-        if (other.transform.tag == "NPC") other.GetComponent<NPC_Core>().TakeDamage(100, 0, transform.position, STATUS_EFFECT.NONE, 0,0);
-        else if (other.transform.tag == "Participant") other.GetComponent<Player_Core>().TakeDamage(100, 0, transform.position, STATUS_EFFECT.NONE, 0,0);
+        if (other.transform.tag == "NPC") other.GetComponent<NPC_Core>().Death();
+        else if (other.transform.tag == "Participant") other.GetComponent<Player_Core>().Death();
     }
 }
