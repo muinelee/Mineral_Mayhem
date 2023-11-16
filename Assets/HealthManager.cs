9846861d6b6cc46875c8b19ec7c826f9d45b7a3d
Fernicious007 <fernandmunyangoga@gmail.com>
2023-11-15 19:38:59 -0800

    Health bar
    Health bar

---
+++ Assets/HealthManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthManager : FloatingHealthBar 
+{
+
+    void Start()
+    {
+
+    }
+
+    public void Update()
+    {
+    }
+}
+      
+    
+ 
+
