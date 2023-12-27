d442c6b2ad44948ea7d0fb25c3f11a4e272c74ee
rcuartero <106171055+rcuartero@users.noreply.github.com>
2023-12-27 17:49:55 -0500

    Created NetworkPlayer_Energy script, and created energy bar
    Created NetworkPlayer_Energy script that can be filled over an adjustable amount of time.
    
    Created energy bar UI element.
    
    Script, and UI are not yet linked

--- Assets/Scripts/NetworkPlayer UI/NetworkPlayer_InGameUI.cs
+++ Assets/Scripts/NetworkPlayer UI/NetworkPlayer_InGameUI.cs
@@ -7,9 +7,9 @@ using Fusion;
 
 public class NetworkPlayer_InGameUI : NetworkBehaviour
 {   
-    [SerializeField] private SO_NetworkAttack qAttack;
-    [SerializeField] private SO_NetworkAttack eAttack;
-    [SerializeField] private SO_NetworkDash dash;
+    private SO_NetworkAttack qAttack;
+    private SO_NetworkAttack eAttack;
+    private SO_NetworkDash dash;
 
     private NetworkPlayer_Attack playerAttack;
     private NetworkPlayer_Movement playerMovement;
@@ -17,6 +17,9 @@ public class NetworkPlayer_InGameUI : NetworkBehaviour
 
     [Header("Health bar")]
     [SerializeField] private Slider healthBar;
+
+    [Header("Energy bar")]
+    [SerializeField] private Slider energyBar;
 
     // cooldown stuff
     [Header("Dash Properties")]
@@ -33,6 +36,10 @@ public class NetworkPlayer_InGameUI : NetworkBehaviour
     [SerializeField] private Image eAttackIcon;
     [SerializeField] private Image eImageCooldown;
     [SerializeField] private Text eCooldownText;
+
+    [Header("F (ULT) Attack Properties")]
+    [SerializeField] private Image fAttackIcon;
+    [SerializeField] private Image fImageCooldown;
 
     // cooldown comes from Scriptable Objects passed from local player
 
@@ -51,6 +58,19 @@ public class NetworkPlayer_InGameUI : NetworkBehaviour
         DisplayAbilityCooldown(ref playerAttack.GetEAttackCoolDownTimer(), eImageCooldown, eCooldownText, eAttack.GetCoolDown());
     }
 
+    private void DisplayHealth()
+    {
+        if (healthBar.value != playerHealth.HP / playerHealth.GetStartingHP() && healthBar.value > 0) healthBar.value = playerHealth.HP / playerHealth.GetStartingHP();
+    }
+
+    public void PrimeUI()
+    {
+        healthBar.interactable = false;
+        dashIcon.sprite = dash.GetDashIcon();
+        qAttackIcon.sprite = qAttack.GetAttackIcon();
+        eAttackIcon.sprite = eAttack.GetAttackIcon();
+    }
+
     private void DisplayAbilityCooldown(ref TickTimer coolDownTimer, Image coolDownImage, Text coolDownText, float maxCoolDown)
     {
         //display the cooldown
@@ -64,18 +84,6 @@ public class NetworkPlayer_InGameUI : NetworkBehaviour
             coolDownText.text = ((float)coolDownTimer.RemainingTime(Runner)).ToString("F2");
             coolDownImage.fillAmount = (float)coolDownTimer.RemainingTime(Runner) / maxCoolDown;
         }
-    }
-
-    private void DisplayHealth()
-    {
-        if (healthBar.value != playerHealth.HP / playerHealth.GetStartingHP() && healthBar.value > 0) healthBar.value = playerHealth.HP / playerHealth.GetStartingHP();
-    }
-
-    public void PrimeUI()
-    {
-        dashIcon.sprite = dash.GetDashIcon();
-        qAttackIcon.sprite = qAttack.GetAttackIcon();
-        eAttackIcon.sprite = eAttack.GetAttackIcon();
     }
 
     public void SetDash(SO_NetworkDash newDash)
