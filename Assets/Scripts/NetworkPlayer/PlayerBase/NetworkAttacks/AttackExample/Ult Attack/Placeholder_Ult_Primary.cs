b5e6b2a3720141eddf3b7e378705d21b69125e22
rcuartero <106171055+rcuartero@users.noreply.github.com>
2024-01-04 11:29:59 -0500

    Ult tracks projectiles being spawned
    Transferred logic to be calculated from first object spawned from Ult

--- Assets/Scripts/NetworkPlayer/PlayerBase/NetworkAttacks/AttackExample/Ult Attack/Placeholder_Ult_Primary.cs
+++ Assets/Scripts/NetworkPlayer/PlayerBase/NetworkAttacks/AttackExample/Ult Attack/Placeholder_Ult_Primary.cs
@@ -3,25 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using Fusion;
 
-public class Placeholder_Ult_Primary : NetworkBehaviour
+public class Placeholder_Ult_Primary : NetworkAttack_Base
 {
-    // Fires a cone of secondary attacks in front of 
+    // Fires a cone of secondary attacks in front of player 
 
-    [Header("Secondary Properties")]
-    [SerializeField] private NetworkObject ultSecondaryPrefab;
-    [SerializeField] private float secondaryNum;
+    [Header("Projectile Properties")]
+    [SerializeField] private NetworkObject projectilePrefab;
+    [SerializeField] private float projectileNum;
     [SerializeField] private float angleOfCone;
+    [SerializeField] private float projectileRadius;
+    private List<Transform> projectileList = new List<Transform>();
 
     [Header("List of objects hit")]
     [SerializeField] private List<Hitbox> objectsHit;         // Check if object is in the objectsHit list. If not, do damage and add to list 
 
-    private void Start()
-    {
-        SpawnAttacks();
-    }
-
     public override void Spawned()
     {
+        if (!Object.HasStateAuthority) return;
+
         SpawnAttacks();
     }
 
@@ -29,17 +28,19 @@ public class Placeholder_Ult_Primary : NetworkBehaviour
     {
         float startOfConeAngle = - (angleOfCone / 2);
 
-        if (secondaryNum == 1) Runner.Spawn(ultSecondaryPrefab, transform.position + Vector3.up, transform.rotation, Object.InputAuthority);
-
-        else
+        if (projectileNum > 1)
         {
-            for (int i = 0; i < secondaryNum; i++)
+            for (int i = 0; i < projectileNum; i++)
             {
-                float angleOffset = startOfConeAngle + ((angleOfCone / (secondaryNum - 1)) * i);
-                NetworkObject spawnedSecondary = Runner.Spawn(ultSecondaryPrefab, transform.position + Vector3.up, Quaternion.Euler(transform.forward + Vector3.up * angleOffset), Object.InputAuthority);
-                spawnedSecondary.GetComponent<Placeholder_Ult_Secondary>().SetParent(this);
+                float angleOffset = startOfConeAngle + ((angleOfCone / (projectileNum - 1)) * i);
+                NetworkObject spawnedSecondary = Runner.Spawn(projectilePrefab, transform.position + Vector3.up, transform.rotation * Quaternion.Euler(Vector3.up * angleOffset), Object.InputAuthority);
+                projectileList.Add(spawnedSecondary.transform);
             }
         }
+
+        else if (projectileNum == 1) Runner.Spawn(projectilePrefab, transform.position + Vector3.up, transform.rotation, Object.InputAuthority);
+
+        else Debug.Log("This ult does not produce any projectiles");
     }
 
     public bool HitboxInObjectsHit(Hitbox objectHit)
@@ -49,6 +50,16 @@ public class Placeholder_Ult_Primary : NetworkBehaviour
         {
             objectsHit.Add(objectHit);
             return false;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (projectileList.Count < 1) return;
+
+        foreach (Transform projectile in projectileList)
+        {
+            Gizmos.DrawSphere(projectile.position, projectileRadius);
         }
     }
 }
--- Assets/Scripts/NetworkPlayer/PlayerBase/NetworkAttacks/AttackExample/Ult Attack/Placeholder_Ult_Secondary.cs
+++ Assets/Scripts/NetworkPlayer/PlayerBase/NetworkAttacks/AttackExample/Ult Attack/Placeholder_Ult_Secondary.cs
@@ -3,33 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Placeholder_Ult_Secondary : NetworkAttack_Base
+public class Placeholder_Ult_Secondary : NetworkBehaviour
 {
-    [Header("Placeholder Primary")]
-    private Placeholder_Ult_Primary parent;
-
-    [Header("Lifetime duration")]
-    [SerializeField] private float lifetimeDuration;
-    private TickTimer timer = TickTimer.None;
-
     [Header("Movement Properties")]
     [SerializeField] private float moveSpeed;
-    private NetworkTransform networkTransform;
+    private NetworkRigidbody networkRigidBody;
 
     public override void Spawned()
     {
-        timer = TickTimer.CreateFromSeconds(Runner, lifetimeDuration);
-        networkTransform = GetComponent<NetworkTransform>();
+        networkRigidBody = GetComponent<NetworkRigidbody>();
     }
 
     public override void FixedUpdateNetwork()
     {
-        networkTransform.transform.Translate(transform.forward * moveSpeed * Runner.DeltaTime);
-        if (timer.Expired(Runner)) Runner.Despawn(GetComponent<NetworkObject>());
-    }
-
-    public void SetParent(Placeholder_Ult_Primary primary)
-    {
-        parent = primary;
+        networkRigidBody.Rigidbody.AddForce(transform.forward * moveSpeed);
     }
 }
