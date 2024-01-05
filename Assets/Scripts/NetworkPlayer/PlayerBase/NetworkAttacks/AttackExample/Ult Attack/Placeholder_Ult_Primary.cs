3bb59f4c893331408799a4fa175e94c17b6c2268
rcuartero <106171055+rcuartero@users.noreply.github.com>
2024-01-05 10:25:11 -0500

    Completed placeholder ult
    Placeholder ult is now able to damage host and client as intended.
    
    Ult able to damage individual players once during each activation.

--- Assets/Scripts/NetworkPlayer/PlayerBase/NetworkAttacks/AttackExample/Ult Attack/Placeholder_Ult_Primary.cs
+++ Assets/Scripts/NetworkPlayer/PlayerBase/NetworkAttacks/AttackExample/Ult Attack/Placeholder_Ult_Primary.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Fusion;
+using Unity.Entities.UniversalDelegates;
 
 public class Placeholder_Ult_Primary : NetworkAttack_Base
 {
@@ -15,13 +16,22 @@ public class Placeholder_Ult_Primary : NetworkAttack_Base
     private List<Transform> projectileList = new List<Transform>();
 
     [Header("List of objects hit")]
-    [SerializeField] private List<Hitbox> objectsHit;         // Check if object is in the objectsHit list. If not, do damage and add to list 
+    [SerializeField] private LayerMask playerLayer;
+    [SerializeField] private List<Hitbox> objectsHit = new List<Hitbox>();         // Check if object is in the objectsHit list. If not, do damage and add to list
+    [SerializeField] private List<LagCompensatedHit> hits = new List<LagCompensatedHit>();
 
     public override void Spawned()
     {
         if (!Object.HasStateAuthority) return;
 
         SpawnAttacks();
+    }
+
+    public override void FixedUpdateNetwork()
+    {
+        if (!Object.HasStateAuthority) return;
+
+        DealDamaage();
     }
 
     private void SpawnAttacks()
@@ -41,6 +51,25 @@ public class Placeholder_Ult_Primary : NetworkAttack_Base
         else if (projectileNum == 1) Runner.Spawn(projectilePrefab, transform.position + Vector3.up, transform.rotation, Object.InputAuthority);
 
         else Debug.Log("This ult does not produce any projectiles");
+    }
+
+    private void DealDamaage()
+    {
+        foreach (Transform projectile in projectileList)
+        {
+            Runner.LagCompensation.OverlapSphere(projectile.position, projectileRadius, player: Object.InputAuthority, hits, playerLayer, HitOptions.IgnoreInputAuthority);
+            foreach (LagCompensatedHit hit in hits)
+            {
+                if (objectsHit.Contains(hit.Hitbox)) continue;
+
+                objectsHit.Add(hit.Hitbox);
+                Debug.Log($"This was hit: {hit.GameObject.name}");
+
+                NetworkPlayer_Health healthHandler = hit.GameObject.GetComponentInParent<NetworkPlayer_Health>();
+
+                if (healthHandler) healthHandler.OnTakeDamage(damage);
+            }
+        }
     }
 
     public bool HitboxInObjectsHit(Hitbox objectHit)
