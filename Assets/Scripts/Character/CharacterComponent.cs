f87b72c011df6664bacf90672c735dcd0d5a643b
GrizzlyJer <88900491+zeGrizzlyJer@users.noreply.github.com>
2024-02-27 12:43:25 -0500

    Fleshing Out Character Component
    Added extra virtual functions hooks

--- Assets/Scripts/Character/CharacterComponent.cs
+++ Assets/Scripts/Character/CharacterComponent.cs
@@ -7,10 +7,41 @@ public class CharacterComponent : NetworkBehaviour
 {
     public CharacterEntity Character { get; private set; }
 
+    /// <summary>
+    /// Called when the character entity is initialized
+    /// </summary>
     public virtual void Init(CharacterEntity character)
     {
         Character = character;
     }
 
-    //  Can implement any other virtual functions we might need the character components to have
+    /// <summary>
+    /// Called when a player takes damage
+    /// </summary>
+    public virtual void OnHit(float x) { }
+
+    /// <summary>
+    /// Called when a player gets healed
+    /// </summary>
+    public virtual void OnHeal(float x) { }
+
+    /// <summary>
+    /// Called when a player gets afflicted (Parameters need to be changed when statuses will be implemented, most likely)
+    /// </summary>
+    public virtual void OnStatused() { }
+
+    /// <summary>
+    /// Called when a player dies
+    /// </summary>
+    public virtual void OnCharacterDeath() { }
+
+    /// <summary>
+    /// Called when the round is complete
+    /// </summary>
+    public virtual void OnRoundComplete() { }
+
+    /// <summary>
+    /// Called when a character picks something up
+    /// </summary>
+    public virtual void OnPickup() { }
 }
