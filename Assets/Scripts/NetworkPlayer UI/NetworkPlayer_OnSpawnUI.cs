6ea7e3768ea78bc793b4c369968f3031dbee642a
rcuartero <106171055+rcuartero@users.noreply.github.com>
2024-03-05 15:09:56 -0500

    Made changes to cyrstra luna character entity

--- Assets/Scripts/NetworkPlayer UI/NetworkPlayer_OnSpawnUI.cs
+++ Assets/Scripts/NetworkPlayer UI/NetworkPlayer_OnSpawnUI.cs
@@ -71,6 +71,7 @@ public class NetworkPlayer_OnSpawnUI : NetworkBehaviour
         if (Object.HasInputAuthority)
         {
             Destroy(playerUI);
+            Destroy(floatingHealthBar);
         }
     }
 }
