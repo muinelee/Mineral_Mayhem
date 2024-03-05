cb141e60a3641c769858cf82dbc349207ee6dd4a
GrizzlyJer <88900491+zeGrizzlyJer@users.noreply.github.com>
2024-03-05 18:35:57 -0500

    I Typo'd

--- Assets/Scripts/CharacterSelect/CharacterSelect.cs
+++ Assets/Scripts/CharacterSelect/CharacterSelect.cs
@@ -52,7 +52,7 @@ public class CharacterSelect : NetworkBehaviour
 
         // Needing for removing monobehaviour HUD before RPC call
         NetworkPlayer player = NetworkPlayer.Players[index];
-        if (characterLookup.ContainsKey(player) == false)
+        if (characterLookup.ContainsKey(player) == true)
         {
             Destroy(NetworkPlayer.Local.GetComponent<NetworkPlayer_OnSpawnUI>().playerUI.gameObject);
         }
