3a254344c5da91321c20d7f5c45f104f6f4b5e5e
Rae Leda <veronica.rleda@gmail.com>
2024-03-24 13:32:31 -0400

    Init RoundManager

---
+++ Assets/Scripts/Managers/RoundManager.cs
@@ -0,0 +1,41 @@
+using Fusion;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundManager : NetworkBehaviour 
+{
+    private bool isRoundEnd;
+    private int redPlayers;
+    private int bluePlayers;
+
+    private int redRoundsWon;
+    private int blueRoundsWon;
+    private int maxRounds; 
+
+
+    private void RedWinsRound()
+    {
+
+    }
+
+    private void BlueWinsRound()
+    {
+
+    }
+    
+    public void RedPlayersDies()
+    {
+
+    }
+
+    public void BluePlayersDies()
+    {
+
+    }
+
+    public void CheckMatchEnd()
+    {
+
+    }
+}
