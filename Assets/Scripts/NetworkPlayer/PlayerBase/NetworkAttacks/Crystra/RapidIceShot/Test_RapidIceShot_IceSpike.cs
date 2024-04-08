f45b7d4bebda41f7c02cbe2d21d497f2a33da9ac
rcuartero <106171055+rcuartero@users.noreply.github.com>
2024-04-08 10:04:56 -0400

    Check Fix

--- Assets/Scripts/NetworkPlayer/PlayerBase/NetworkAttacks/Crystra/RapidIceShot/Test_RapidIceShot_IceSpike.cs
+++ Assets/Scripts/NetworkPlayer/PlayerBase/NetworkAttacks/Crystra/RapidIceShot/Test_RapidIceShot_IceSpike.cs
@@ -2,28 +2,17 @@ using Fusion;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.ProBuilder;
 using UnityEngine.UIElements;
-using static Fusion.NetworkCharacterController;
 
 public class Test_RapidIceShot_IceSpike : NetworkBehaviour
 {
     [Header("Movement Properties")]
     [SerializeField] private float speed;
+    [SerializeField] private float lifetime;
+    private float lifeTimer = 0;
     [SerializeField] private float offset;
 
-    [Header("Lifetime Properties")]
-    [SerializeField] private float lifetimeDuration;
-    private TickTimer lifetimeTimer = TickTimer.None;
-
-    [Header("Damage Properties")]
-    [SerializeField] private float damage;
-    [SerializeField] private float radius;
-    [SerializeField] private float knockback;
-    [SerializeField] private LayerMask playerLayer;
-    private List<LagCompensatedHit> hits = new List<LagCompensatedHit>();
-
-    public override void Spawned()
+    private void Start()
     {
         float offsetX = Random.Range(-offset, offset);
         float offsetY = Random.Range(0, offset);
@@ -31,53 +20,16 @@ public class Test_RapidIceShot_IceSpike : NetworkBehaviour
         Vector3 offsetVector = new Vector3(offsetX, offsetY, 0);
 
         transform.Translate(offsetVector);
-
-        lifetimeTimer = TickTimer.CreateFromSeconds(Runner, lifetimeDuration);
     }
 
-    public override void FixedUpdateNetwork()
+    // Update is called once per frame
+    void FixedUpdate()
     {
         // move
         transform.Translate(Vector3.forward * speed);
 
-        CheckForHits();
-
         // manage lifetime
-        if (lifetimeTimer.Expired(Runner))
-        {
-            EndAttack();
-        }
-    }
-
-    public void CheckForHits()
-    {
-        // search for objects hit and deal damage if necessary
-        Runner.LagCompensation.OverlapSphere(transform.position, radius, player: Object.InputAuthority, hits, playerLayer, HitOptions.IgnoreInputAuthority);
-
-        if (hits.Count == 0) return;
-
-        foreach (LagCompensatedHit hit in hits)
-        {
-            CharacterEntity character = hit.GameObject.GetComponentInParent<CharacterEntity>();
-
-            if (!character) continue;
-
-            character.OnHit(damage);
-            character.Health.OnKnockback(knockback, transform.position);
-            Debug.Log("Should be applying knockback");
-
-            EndAttack();
-        }
-    }
-
-    public void EndAttack()
-    {
-        lifetimeTimer = TickTimer.None;
-        Runner.Despawn(Object);
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawSphere(transform.position, radius);
+        lifeTimer += Time.deltaTime;
+        if (lifeTimer > lifetime) Destroy(gameObject);
     }
 }
