7406f2eda6fab25ac3d4d1ca8ae94ec5c068dffa
RuniteOre <46429602+RuniteOre@users.noreply.github.com>
2024-04-12 17:50:06 -0400

    first round on multiplier
    first pass
    
    Co-Authored-By: TinyGiantt <122888009+TinyGiantt@users.noreply.github.com>

--- Assets/Characters/Crystra/Abilities/RapidIceShot/Test_RapidIceShot_IceSpike.cs
+++ Assets/Characters/Crystra/Abilities/RapidIceShot/Test_RapidIceShot_IceSpike.cs
@@ -17,6 +17,10 @@ public class Test_RapidIceShot_IceSpike : NetworkAttack_Base
     List<LagCompensatedHit> hits = new List<LagCompensatedHit>();
     [SerializeField] private LayerMask collisionLayer;
 
+    // Damage properties
+    [Header("Damage Properties")]
+    [SerializeField] private float damageMultiplier = 1.2f;
+
     private void Start()
     {
         float offsetX = Random.Range(-offset, offset);
@@ -42,16 +46,42 @@ public class Test_RapidIceShot_IceSpike : NetworkAttack_Base
     
 
     protected override void DealDamage() {
+        float totalDamage;
+        int totalDamageInt;
+        //hit signature to check 
         Runner.LagCompensation.OverlapSphere(transform.position, radius, player: Object.InputAuthority, hits, collisionLayer, HitOptions.IgnoreInputAuthority);
 
-
+        //loop to check for hits
         for (int i = 0; i < hits.Count; i++) {
             Debug.Log($"Did we hit a hitbox? {hits[i].Hitbox}");
             NetworkPlayer_Health healthHandler = hits[i].GameObject.GetComponentInParent<NetworkPlayer_Health>();
 
-            if (healthHandler) healthHandler.OnTakeDamage(damage);
-            Runner.Despawn(GetComponent<NetworkObject>());
+
+            if (healthHandler) {
+                //if its the first hit, ignore the multiplier
+                if (hits.Count <= 1) {
+                    //total damage equal to damage
+                    totalDamage = damage;
+                    //continue to next hit
+                    continue;
+                }
+                else {
+                    //if not first hit, multiply the damage
+                    totalDamage = damage * damageMultiplier;
+                }
+                //send damage to health handler as int
+                healthHandler.OnTakeDamage((int)totalDamage);
+                //debug log to check if damage is being dealt
+                Debug.Log($"Dealt {totalDamage} damage to {healthHandler.gameObject.name}");
+                Runner.Despawn(GetComponent<NetworkObject>());
+            }
         }
 
+    }
+
+    private int MultiplyDamage() {
+        int incomingDamage;
+        int newDamage = 1;
+        return newDamage;
     }
 } 
