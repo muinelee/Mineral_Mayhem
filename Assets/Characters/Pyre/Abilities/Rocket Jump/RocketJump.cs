cd8702433ff504b9b47d21acb8a0b192d331b636
rcuartero <106171055+rcuartero@users.noreply.github.com>
2024-04-15 16:21:09 -0400

    Created prefab for Rocket Jump, Added animation, and SO to attack  folder
    Created prefab
    Linked prefab to SO
    Added animation into Pyre
    Set up animation events for reset and spawn

---
+++ Assets/Characters/Pyre/Abilities/Rocket Jump/RocketJump.cs
@@ -0,0 +1,48 @@
+using Fusion;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketJump : NetworkAttack_Base
+{
+    private Transform playerSource;
+
+    [Header("Attack Properties")]
+    [SerializeField] private float radius;    
+    [SerializeField] private float forceForward;
+    [SerializeField] private float forceUpward;
+    [SerializeField] private float landingOffset;
+    [SerializeField] private LayerMask collisionLayer;
+
+    private List<LagCompensatedHit> hits;
+
+    private NetworkRigidbody rb;
+    [SerializeField] private float gravityScale;
+
+    public override void Spawned()
+    {
+        base.Spawned();
+        
+        if (!Runner.IsServer) return;
+
+        Runner.LagCompensation.OverlapSphere(transform.position, radius, player: Object.InputAuthority, hits, collisionLayer);
+
+        for (int i = 0; i < hits.Count; i++)
+        {
+            CharacterEntity character = hits[i].GameObject.GetComponentInParent<CharacterEntity>();
+
+            if (character)
+            {
+                if (character.Object.InputAuthority == Object.InputAuthority)
+                {
+                    Debug.Log("This is not the player you're looking for");
+                    continue;
+                }
+
+                playerSource = character.transform;
+                rb = character.Rigidbody;
+                rb.Rigidbody.AddForce(transform.forward * forceForward + transform.up * forceUpward);
+            }
+        }
+    }
+}
