88b0d177f9adf4fbc0f134a6f8b9bf8772428a74
RuniteOre <46429602+RuniteOre@users.noreply.github.com>
2024-04-19 15:57:49 -0400

    Full Functionality complete
    Damage, Knockback, and Slow implemented in Coldhamehameha
    
    Co-Authored-By: TinyGiantt <122888009+TinyGiantt@users.noreply.github.com>

--- Assets/Characters/Crystra/Abilities/Coldhamehameha/Coldmehameha.cs
+++ Assets/Characters/Crystra/Abilities/Coldhamehameha/Coldmehameha.cs
@@ -8,18 +8,19 @@ public class Coldmehameha : NetworkAttack_Base
 {
     //componets for getting attack objects
     [Header("Attack Properties")]
-    //used for OverlapSphere, will be removed when swapped to OverlapBox
-    [SerializeField] private float attradius; ///////////////////////////
     //lifetime of the attack
     [SerializeField] private float lifetime;
     //ticktimer to work with lifetime
     private TickTimer lifeTimer = TickTimer.None;
     //spawn offset
 
+    [Header("Spawn Properties")]
+    public int offset;
 
     CharacterEntity character;
+    //extends for the overlap box
     public Vector3 extends = new Vector3(0.5f, 0.5f, 6f);
-
+    //list for hits
     List<LagCompensatedHit> hits = new List<LagCompensatedHit>();
     [SerializeField] private LayerMask collisionLayer;
 
@@ -35,8 +36,11 @@ public class Coldmehameha : NetworkAttack_Base
         for (int i = 0; i < hits.Count; i++) {
             //get character entity as parent of hit object
             character = hits[i].GameObject.GetComponentInParent<CharacterEntity>();
+            //set parent of the object to the character entity
             this.transform.SetParent(character.transform);
         }
+        //offset spawn position to match hands
+        transform.position += transform.forward * offset;
     }
     void FixedUpdate()
     {
@@ -58,14 +62,11 @@ public class Coldmehameha : NetworkAttack_Base
         Runner.Despawn(Object);
     }
 
-    //private void OnDrawGizmos() {
-    //    //draw gizmos for the attack
-    //    Gizmos.DrawCube((this.transform.position + (transform.forward * 3)), extends);
-    //}
-
     protected override void DealDamage()
     {
+        //get the character rotation
         Quaternion rotation = Quaternion.LookRotation(character.transform.forward);
+        //signature for hits using the characterposition + (rotation * 3(half size of the box)
         Runner.LagCompensation.OverlapBox((character.transform.position + (character.transform.forward * 3)), extends, rotation, player: Object.InputAuthority, hits, collisionLayer, HitOptions.IgnoreInputAuthority);
         //loop to check for hits
         foreach (LagCompensatedHit hit in hits) {
@@ -73,8 +74,6 @@ public class Coldmehameha : NetworkAttack_Base
             IHealthComponent healthComponent = hit.GameObject.GetComponentInParent<IHealthComponent>();
             //if health component is not null
             if (healthComponent != null) {
-                //debug log for testing
-                Debug.Log("Dealing Damage");
                 //if health component is not dead, or if the team is the same, continue
                 if (healthComponent.isDead || CheckIfSameTeam(healthComponent.GetTeam())) continue;
                 //call the on take damage function from the health component
@@ -84,6 +83,12 @@ public class Coldmehameha : NetworkAttack_Base
             }
 
             CharacterEntity characterEntity = hit.GameObject.GetComponentInParent<CharacterEntity>();
+
+            if (statusEffectSO.Count > 0 && characterEntity) {
+                foreach (StatusEffect status in statusEffectSO) {
+                    characterEntity.OnStatusBegin(status);
+                }
+            }
         }
     }
 }
