032c784dfa9171cf5e90009ac350b6f724ca8ea6
Rae Leda <veronica.rleda@gmail.com>
2024-06-06 20:24:51 -0400

    Made Cinematic Prefab

--- Assets/Scripts/Managers/IntroManager.cs
+++ Assets/Scripts/Managers/IntroManager.cs
@@ -3,25 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI; 
-using Unity.VisualScripting;
+using UnityEngine.UI;
 
 public class IntroManager : MonoBehaviour
 {
-    private VideoPlayer videoPlayer; 
-    private float holdTime = 2.0f; 
+    private VideoPlayer videoPlayer;
+    private float holdTime = 2.0f;
     private float currentHoldTime = 0.0f;
-    [SerializeField] private Slider holdTimeSlider; 
+    [SerializeField] private Slider holdTimeSlider;
+
+    private const string IntroSeenKey = "IntroSeen";
 
     private void Start()
     {
+        if (PlayerPrefs.HasKey(IntroSeenKey) && PlayerPrefs.GetInt(IntroSeenKey) == 1)
+        {
+            gameObject.SetActive(false);
+            return;
+        }
+
         videoPlayer = GameObject.Find("IntroVideo").GetComponent<VideoPlayer>();
         videoPlayer.loopPointReached += EndVideo;
 
         if (holdTimeSlider != null)
         {
             holdTimeSlider.minValue = 0;
-            holdTimeSlider.maxValue = 1; 
+            holdTimeSlider.maxValue = 1;
         }
     }
 
@@ -39,16 +46,22 @@ public class IntroManager : MonoBehaviour
         float holdTimePercentage = Mathf.Clamp01(currentHoldTime / holdTime);
 
         if (holdTimeSlider != null) holdTimeSlider.value = holdTimePercentage;
-        if (currentHoldTime >= holdTime) EndVideo(videoPlayer); 
+        if (currentHoldTime >= holdTime) EndVideo(videoPlayer);
     }
 
     private void EndVideo(VideoPlayer vp)
     {
-        SceneManager.LoadScene("Main Menu"); 
+        PlayerPrefs.SetInt(IntroSeenKey, 1);  
+        PlayerPrefs.Save(); 
+        gameObject.SetActive(false);
+        //SceneManager.LoadScene("Main Menu"); 
     }
 
     private void OnDestroy()
     {
-        videoPlayer.loopPointReached -= EndVideo; 
+        if (videoPlayer != null)
+        {
+            videoPlayer.loopPointReached -= EndVideo;
+        }
     }
 }
