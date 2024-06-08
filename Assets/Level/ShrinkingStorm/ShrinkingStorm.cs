7496f1f508f142fb68ccad4c2b3d9eb8394adef1
rcuartero <106171055+rcuartero@users.noreply.github.com>
2024-06-08 15:57:41 -0400

    Storm value change

--- Assets/Level/ShrinkingStorm/ShrinkingStorm.cs
+++ Assets/Level/ShrinkingStorm/ShrinkingStorm.cs
@@ -163,7 +163,6 @@ public class ShrinkingStorm : NetworkAttack_Base {
     [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
     private void RPC_ResetStorm()
     {
-        Debug.Log("The storm's scale should have been reset");
         this.transform.localScale = startScale;
         this.ShrinkStorm();
         this.stormVFX.Stop();
