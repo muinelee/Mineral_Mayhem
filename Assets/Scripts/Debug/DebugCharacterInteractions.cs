2e271821b964f1fc85cd13d4eb3af2ca42b7dfe9
GrizzlyJer <88900491+zeGrizzlyJer@users.noreply.github.com>
2024-06-08 13:04:00 -0400

    Debug Stuff (H to Hide! Adding a hint for that next)

---
+++ Assets/Scripts/Debug/DebugCharacterInteractions.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugCharacterInteractions : MonoBehaviour
+{
+    [SerializeField] private Button HealButton;
+    [SerializeField] private Button DmgButton;
+
+    private void Start()
+    {
+        if (HealButton)
+        {
+            HealButton.onClick.AddListener(HealCharacter);
+        }
+        if (DmgButton)
+        {
+            DmgButton.onClick.AddListener(DamageCharacter);
+        }
+    }
+
+    public void HealCharacter()
+    {
+        CharacterEntity character = NetworkPlayer.Local.Avatar.Character;
+        if (!character)
+        {
+            Debug.Log("No CharacterEntity found - get good.");
+            return;
+        }
+        character.OnHeal(1f);
+    }
+
+    public void DamageCharacter()
+    {
+        CharacterEntity character = NetworkPlayer.Local.Avatar.Character;
+        if (!character)
+        {
+            Debug.Log("No CharacterEntity found - get good.");
+            return;
+        }
+        character.OnHit(1f, false);
+    }
+}
